40fc8906a3f6a482f5923d8ba32f86e0121ae1b0
Romero-Ezequiel <romerorezequiel@gmail.com>
2020-03-05 16:26:23 -0300

    Configuración de la dirección de la BD

---
+++ src/config/database.js
@@ -0,0 +1,6 @@
+/* Defino un objeto y lo exporto, el cual va a tener una propiedad url y esta
+propiedad va a contener la direccion de la base de datos de mongoDB local y
+el nombre de la base de datos*/
+module.exports = {
+	'url': 'mongodb://localhost/login-node'
+};
